d2e91cef4be1bcd810ab1c7e393c37ba91d1842b
Sean Murphy <sean@iamseanmurphy.com>
2026-09-10 21:38:07 +0000

    4.0.14

--- woo-stripe-payment/vendor/composer/installed.php
+++ woo-stripe-payment/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '4.0.13.x-dev',
-        'version' => '4.0.13.9999999-dev',
-        'reference' => 'e5e87197009ba5800a0c76778d750c5e7517e1f6',
+        'pretty_version' => '4.0.14.x-dev',
+        'version' => '4.0.14.9999999-dev',
+        'reference' => '4ee7a0fb0f11ce43d05dbb44f2d922abd225ee64',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '4.0.13.x-dev',
-            'version' => '4.0.13.9999999-dev',
-            'reference' => 'e5e87197009ba5800a0c76778d750c5e7517e1f6',
+            'pretty_version' => '4.0.14.x-dev',
+            'version' => '4.0.14.9999999-dev',
+            'reference' => '4ee7a0fb0f11ce43d05dbb44f2d922abd225ee64',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
